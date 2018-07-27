827f1af6053d57abfe8fc1db2e7ae5080e45f559
juchan96 <ghkdwncks96@gmail.com>
2018-07-27 15:08:28 +0900

    refactor: 27line for 반복 문을 for...of 반복문으로 수정함

--- step2.js
+++ step2.js
@@ -19,55 +19,32 @@ class ArrayParser {
     return this.parseData(result);
   }
   //분석한 데이터를 반환해주는 함수
-  // parseData(splitData) {
-  //   let isTrue = true;
-  //   let count = 0;
-  //   for (let n = 0; n < splitData.length; n++) {
-  //     const dataSample = new dataSampleClass();
-  //     if (splitData[n] === "") {
-  //       count++;
-  //       count % 2 === 1 ? isTrue = false : isTrue = true;
-  //       continue;
-  //     }
-  //     else if (splitData[n] !== "") {
-  //       !isNaN(splitData[n]) ? dataSample.item.type = "number" : dataSample.item.type = "not a number";
-  //       dataSample.item.value = splitData[n];
-  //     }
-  //     if (!isTrue) {
-  //       const dataSample1 = new dataSampleClass();
-  //       dataSample1.item.type = "array"
-  //       dataSample1.item.value = "ArrayObject"
-  //       dataSample1.item.child.push(dataSample.item);
-  //       this.item.child.push(dataSample1.item)
-  //     } else this.item.child.push(dataSample.item);
-  //   }
-  //   return this.item;
-  // }
+  //중첩된 배열에서 '[' 괄호가 나오면 count를해서 isTrue에 false또는 true를 대입한다.
+  //isTrue가 false = 중첩된 배열 이라는 뜻, 그 배열은 새로운 객체에 담는다.
   parseData(splitData) {
     let isTrue = true;
     let count = 0;
-    for (let n = 0; n < splitData.length; n++) {
+    for (let value of splitData) {
       const dataSample = new dataSampleClass();
-      if (splitData[n] === "") {
+      if (value === "") {
         count++;
         count % 2 === 1 ? isTrue = false : isTrue = true;
         continue;
       }
-      else if (splitData[n] !== "") {
-        !isNaN(splitData[n]) ? dataSample.item.type = "number" : dataSample.item.type = "not a number";
-        dataSample.item.value = splitData[n];
+      else if (value !== "") {
+        !isNaN(value) ? dataSample.item.type = "number" : dataSample.item.type = "not a number";
+        dataSample.item.value = value;
       }
       if (!isTrue) {
         const dataSample1 = new dataSampleClass();
-        dataSample1.item.type = "array"
-        dataSample1.item.value = "ArrayObject"
+        dataSample1.item.type = "array";
+        dataSample1.item.value = "ArrayObject";
         dataSample1.item.child.push(dataSample.item);
-        this.item.child.push(dataSample1.item)
+        this.item.child.push(dataSample1.item);
       } else this.item.child.push(dataSample.item);
     }
     return this.item;
   }
-
 }
 
 class dataSampleClass {
@@ -82,4 +59,4 @@ class dataSampleClass {
 
 const parseStr = new ArrayParser();
 console.log(parseStr.splitData("[123,[22],33]"));
-console.log(parseStr.splitData("[123,[22],33,[1,2,3,4,5]]"));
+// console.log(parseStr.splitData("[123,[22],33,[1,2,3,4,5]]"));
